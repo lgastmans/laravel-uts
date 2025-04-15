d1d602b767416df2257523b4aa0b8c69af4174a1
Luk Gastmans <lgastmans@gmail.com>
2025-04-15 16:16:08 +0530

    sync updates

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Where to redirect users after login.
+     */
+    public const HOME = '/dashboard';
+
+    public function boot(): void
+    {
+        parent::boot();
+
+        $this->routes(function () {
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+        });
+    }
+}
