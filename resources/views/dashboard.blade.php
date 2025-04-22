60de73943ce29c59ae80f72485169f5675ba753e
Luk Gastmans <lgastmans@gmail.com>
2025-04-22 06:35:24 +0530

    updates

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,19 +6,7 @@
     </x-slot>
 
     <div class="p-4">
-        
         {{--        
-        <div id="bulkAlertBox" class="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert" style="display: none;">
-            <svg class="shrink-0 inline w-4 h-4 me-3 mt-[2px]" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-            </svg>
-            <div>
-                <span id="bulkAlertTitle" class="font-medium">Messages:</span>
-                <ul id="bulkAlertList" class="mt-1.5 list-disc list-inside"></ul>
-            </div>
-        </div>
-        --}}
-
         <div id="bulkAlertBox"
             class="relative flex p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
             role="alert"
@@ -44,8 +32,33 @@
                 </svg>
             </button>
         </div>
+        --}}
 
+        <div id="bulkAlertBox"
+            class="relative flex p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50"
+            role="alert"
+            style="display: none;">
+            
+            <svg class="shrink-0 inline w-4 h-4 me-3 mt-[2px]" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                viewBox="0 0 20 20">
+                <path
+                    d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+            </svg>
 
+            <div>
+                <span id="bulkAlertTitle" class="font-medium">Messages:</span>
+                <ul id="bulkAlertList" class="mt-1.5 list-disc list-inside"></ul>
+            </div>
+
+            <!-- Close button -->
+            <button onclick="document.getElementById('bulkAlertBox').style.display='none'"
+                class="absolute top-2 right-2 text-red-800 hover:text-red-900">
+                <svg class="w-4 h-4" fill="none" stroke="currentColor" stroke-width="2"
+                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+        </div>
 
         <livewire:bills-table />
 
